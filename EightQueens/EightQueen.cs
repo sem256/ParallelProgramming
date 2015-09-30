57c7480dd16baf11c5d15a02b67c0274decef4f5
sem256 <motia256@gmail.com>
2015-09-30 17:17:23 +0300

    add EightQueen.cs

---
+++ EightQueens/EightQueen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EightQueens
+{
+    class EightQueen
+    {
+        public int Number { get; set; }
+        public EightQueen(int size)
+        {
+            this.Number = size;
+        }
+        public int GetSolution()
+        {
+            int line = 0, pos = -1, count = 0;
+            int[] positions = new int[Number];
+
+            for (int m = 0; m < positions.Length; m++)
+                positions[m] = -1;
+            while (true)
+            {
+                for (pos = positions[line] + 1; pos < Number; pos++)
+                {
+                    int i = 0;
+                    for (i = 0; i < line; i++)
+                    {
+                        int dis = line - i;
+                        if (positions[i] == pos || positions[i] == pos + dis || positions[i] == pos - dis)
+                            break;
+                    }
+                    if (i == line)
+                    {
+                        positions[line] = pos;
+                        line++;
+                        if (line == Number)
+                        {
+                            count++;
+                            line--;
+                        }
+                        else
+                            break;
+                    }
+                }
+                if (pos == Number)
+                {
+                    if (line == 0)
+                        break;
+                    else
+                    {
+                        positions[line] = -1;
+                        line--;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
